78a666dc6d880daf1051dcef8c976bab34a2e7b5
변찬우 <byun00106@gmail.com>
2024-04-15 22:14:59 +0900

    feat: add navbar link buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="h-full">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar />
     </main>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = async () => {
   return (
@@ -14,6 +15,28 @@ const Navbar = async () => {
         />
         <p className="text-3xl font-bold">zie</p>
       </aside>
+      <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="#">Products</Link>
+          </li>
+          <li>
+            <Link href="#">Pricing</Link>
+          </li>
+          <li>
+            <Link href="#">Clients</Link>
+          </li>
+          <li>
+            <Link href="#">Resources</Link>
+          </li>
+          <li>
+            <Link href="#">Documentation</Link>
+          </li>
+          <li>
+            <Link href="#">Enterprise</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
