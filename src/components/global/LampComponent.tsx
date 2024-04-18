50d34b623696d26b37129e2a7849d729b97dd7b8
변찬우 <byun00106@gmail.com>
2024-04-18 21:06:02 +0900

    feat: add pricing card components

--- src/components/global/LampComponent.tsx
+++ src/components/global/LampComponent.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
-import { SparklesCore } from "./sparkles";
+import { SparklesCore } from "./Sparkles";
 
 export function LampComponent() {
   return (
