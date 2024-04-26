56c52a21478686019b8a96909aec670f77ceac1f
변찬우 <byun00106@gmail.com>
2024-04-26 21:51:46 +0900

    feat: add sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,27 +1,26 @@
 "use client";
-
-import { menuOptions } from "@/lib/constant";
-import {
-  Tooltip,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@radix-ui/react-tooltip";
-import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ReactNode } from "react";
-
-interface Props {
-  children: ReactNode;
-}
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { menuOptions } from "@/lib/constant";
+import clsx from "clsx";
+import { Separator } from "@/components/ui/separator";
+import { Database, GitBranch, LucideMousePointerClick } from "lucide-react";
+import { ModeToggle } from "../global/ModeToggle";
 
 const Sidebar = () => {
   const pathName = usePathname();
 
   return (
-    <nav className="dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-2">
+    <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
       <div className="flex items-center justify-center flex-col gap-8">
-        <Link className="flex font-bold flex-row" href="/">
+        <Link className="flex font-bold flex-row " href="/">
           fuzzie.
         </Link>
         <TooltipProvider>
@@ -33,9 +32,9 @@ const Sidebar = () => {
                     <Link
                       href={menuItem.href}
                       className={clsx(
-                        "group h-8 w-8 flex items-center justify-center scale-[1.5] rounded-lg p-[3px] cursor-pointer",
+                        "group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer",
                         {
-                          "dark:bg-[#2F006B] bg-[#EEE0FF]":
+                          "dark:bg-[#2F006B] bg-[#EEE0FF] ":
                             pathName === menuItem.href,
                         },
                       )}
@@ -46,10 +45,37 @@ const Sidebar = () => {
                     </Link>
                   </li>
                 </TooltipTrigger>
+                <TooltipContent
+                  side="right"
+                  className="bg-black/10 backdrop-blur-xl"
+                >
+                  <p>{menuItem.name}</p>
+                </TooltipContent>
               </Tooltip>
             </ul>
           ))}
         </TooltipProvider>
+        <Separator />
+        <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <LucideMousePointerClick className="dark:text-white" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch className="text-muted-foreground" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <Database className="text-muted-foreground" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch className="text-muted-foreground" size={18} />
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center flex-col gap-8">
+        <ModeToggle />
       </div>
     </nav>
   );
